607ae5ba3dd8c511cbed0fedd7d44d0c019288f3
Katsunori Tagami <u0001901@login1.cloud.r-ccs.riken.jp>
2026-02-09 18:21:12 +0900

    lud is updated

--- src2/lud-acc/lud.cpp
+++ src2/lud-acc/lud.cpp
@@ -110,7 +110,7 @@ int main ( int argc, char *argv[] )
   /* beginning of timing point */
   stopwatch_start(&sw);
 
-  #pragma acc data tofrom: m[0:(size_t)matrix_dim*matrix_dim])
+  #pragma acc data copy( m[0:(size_t)matrix_dim*matrix_dim])
   {
   int offset;
   int i=0;
@@ -119,194 +119,206 @@ int main ( int argc, char *argv[] )
 
   for (i=0; i < matrix_dim-BLOCK_SIZE; i += BLOCK_SIZE) {
     offset = i;  // add the offset 
-    #pragma acc parallel teams gang(1) thread_limit(BLOCK_SIZE)
+    #pragma acc parallel num_gangs(1) vector_length(BLOCK_SIZE) present(m)
     {
       float shadow[BLOCK_SIZE * BLOCK_SIZE];
-      #pragma omp parallel
-      {
-        int i,j;
-        int tx = omp_get_thread_num() ;
-      
+
+      #pragma acc loop vector
+      for (int tx = 0; tx < BLOCK_SIZE; tx++) {
         size_t array_offset = offset*(size_t)matrix_dim+offset;
-        for(i=0; i < BLOCK_SIZE; i++){
+        for(int i=0; i < BLOCK_SIZE; i++){
           shadow[i * BLOCK_SIZE + tx]=m[array_offset + tx];
           array_offset += matrix_dim;
         }
-        
-        #pragma omp barrier
-        
-        for(i=0; i < BLOCK_SIZE-1; i++) {
-      
+      }
+
+      for(int i=0; i < BLOCK_SIZE-1; i++) {
+
+        #pragma acc loop vector
+        for (int tx = 0; tx < BLOCK_SIZE; tx++) {
           if (tx>i){
-            for(j=0; j < i; j++)
+            for(int j=0; j < i; j++)
               shadow[tx * BLOCK_SIZE + i] -= shadow[tx * BLOCK_SIZE + j] * shadow[j * BLOCK_SIZE + i];
-          shadow[tx * BLOCK_SIZE + i] /= shadow[i * BLOCK_SIZE + i];
+            shadow[tx * BLOCK_SIZE + i] /= shadow[i * BLOCK_SIZE + i];
           }
-      
-          #pragma omp barrier
+        }
+
+        #pragma acc loop vector
+        for (int tx = 0; tx < BLOCK_SIZE; tx++) {
           if (tx>i){
-      
-            for(j=0; j < i+1; j++)
+            for(int j=0; j < i+1; j++)
               shadow[(i+1) * BLOCK_SIZE + tx] -= shadow[(i+1) * BLOCK_SIZE + j]*shadow[j * BLOCK_SIZE + tx];
           }
-          
-          #pragma omp barrier
         }
-      
-        array_offset = (offset+1)*(size_t)matrix_dim+offset;
-        for(i=1; i < BLOCK_SIZE; i++){
+      }
+
+      #pragma acc loop vector
+      for (int tx = 0; tx < BLOCK_SIZE; tx++) {
+        size_t array_offset = (offset+1)*(size_t)matrix_dim+offset;
+        for(int i=1; i < BLOCK_SIZE; i++){
           m[array_offset+tx]=shadow[i * BLOCK_SIZE + tx];
           array_offset += matrix_dim;
         }
       }
     }
 
-    #pragma acc parallel teams num_teams((matrix_dim-i)/BLOCK_SIZE-1) thread_limit(2*BLOCK_SIZE)
     {
-      float dia[BLOCK_SIZE * BLOCK_SIZE];
-      float peri_row[BLOCK_SIZE * BLOCK_SIZE];
-      float peri_col[BLOCK_SIZE * BLOCK_SIZE];
-      #pragma omp parallel
-      {
-         size_t array_offset;
-         int i,j;
-         int idx;
+      int num_peri = (matrix_dim-i)/BLOCK_SIZE-1;
+      #pragma acc parallel loop gang num_gangs(num_peri) vector_length(2*BLOCK_SIZE) present(m)
+      for (int bx = 0; bx < num_peri; bx++) {
+        float dia[BLOCK_SIZE * BLOCK_SIZE];
+        float peri_row[BLOCK_SIZE * BLOCK_SIZE];
+        float peri_col[BLOCK_SIZE * BLOCK_SIZE];
 
-         int  bx = omp_get_team_num();  
-         int  tx = omp_get_thread_num();
+        #pragma acc loop vector
+        for (int tx = 0; tx < 2*BLOCK_SIZE; tx++) {
+          size_t array_offset;
+          int idx;
 
-         if (tx < BLOCK_SIZE) {
-           idx = tx;
-           array_offset = offset*(size_t)matrix_dim+offset;
-           for (i=0; i < BLOCK_SIZE/2; i++){
-           dia[i * BLOCK_SIZE + idx]=m[array_offset+idx];
-           array_offset += matrix_dim;
-           }
-         
-         array_offset = offset*(size_t)matrix_dim+offset;
-         for (i=0; i < BLOCK_SIZE; i++) {
-           peri_row[i * BLOCK_SIZE+ idx]=m[array_offset+(bx+1)*BLOCK_SIZE+idx];
-           array_offset += matrix_dim;
-         }
+          if (tx < BLOCK_SIZE) {
+            idx = tx;
+            array_offset = offset*(size_t)matrix_dim+offset;
+            for (int i=0; i < BLOCK_SIZE/2; i++){
+              dia[i * BLOCK_SIZE + idx]=m[array_offset+idx];
+              array_offset += matrix_dim;
+            }
 
-         } else {
-         idx = tx-BLOCK_SIZE;
-         
-         array_offset = (offset+BLOCK_SIZE/2)*(size_t)matrix_dim+offset;
-         for (i=BLOCK_SIZE/2; i < BLOCK_SIZE; i++){
-           dia[i * BLOCK_SIZE + idx]=m[array_offset+idx];
-           array_offset += matrix_dim;
-         }
-         
-         array_offset = (offset+(bx+1)*BLOCK_SIZE)*(size_t)matrix_dim+offset;
-         for (i=0; i < BLOCK_SIZE; i++) {
-           peri_col[i * BLOCK_SIZE + idx] = m[array_offset+idx];
-           array_offset += matrix_dim;
-         }
-       }
-       #pragma omp barrier
+            array_offset = offset*(size_t)matrix_dim+offset;
+            for (int i=0; i < BLOCK_SIZE; i++) {
+              peri_row[i * BLOCK_SIZE+ idx]=m[array_offset+(bx+1)*BLOCK_SIZE+idx];
+              array_offset += matrix_dim;
+            }
 
-       if (tx < BLOCK_SIZE) { //peri-row
-         idx=tx;
-         for(i=1; i < BLOCK_SIZE; i++){
-           for (j=0; j < i; j++)
-             peri_row[i * BLOCK_SIZE + idx]-=dia[i * BLOCK_SIZE+ j]*peri_row[j * BLOCK_SIZE + idx];
-         }
-       } else { //peri-col
-         idx=tx - BLOCK_SIZE;
-         for(i=0; i < BLOCK_SIZE; i++){
-           for(j=0; j < i; j++)
-             peri_col[idx * BLOCK_SIZE + i]-=peri_col[idx * BLOCK_SIZE+ j]*dia[j * BLOCK_SIZE + i];
-            peri_col[idx * BLOCK_SIZE + i] /= dia[i * BLOCK_SIZE+ i];
-         }
-       }
+          } else {
+            idx = tx-BLOCK_SIZE;
 
-       #pragma omp barrier
-       if (tx < BLOCK_SIZE) { //peri-row
-         idx=tx;
-         array_offset = (offset+1)*(size_t)matrix_dim+offset;
-         for(i=1; i < BLOCK_SIZE; i++){
-           m[array_offset+(bx+1)*BLOCK_SIZE+idx] = peri_row[i*BLOCK_SIZE+idx];
-           array_offset += matrix_dim;
-         }
-       } else { //peri-col
-         idx=tx - BLOCK_SIZE;
-         array_offset = (offset+(bx+1)*BLOCK_SIZE)*(size_t)matrix_dim+offset;
-         for(i=0; i < BLOCK_SIZE; i++){
-           m[array_offset+idx] =  peri_col[i*BLOCK_SIZE+idx];
-           array_offset += matrix_dim;
-         }
-       }
+            array_offset = (offset+BLOCK_SIZE/2)*(size_t)matrix_dim+offset;
+            for (int i=BLOCK_SIZE/2; i < BLOCK_SIZE; i++){
+              dia[i * BLOCK_SIZE + idx]=m[array_offset+idx];
+              array_offset += matrix_dim;
+            }
+
+            array_offset = (offset+(bx+1)*BLOCK_SIZE)*(size_t)matrix_dim+offset;
+            for (int i=0; i < BLOCK_SIZE; i++) {
+              peri_col[i * BLOCK_SIZE + idx] = m[array_offset+idx];
+              array_offset += matrix_dim;
+            }
+          }
+        }
+
+        #pragma acc loop vector
+        for (int tx = 0; tx < 2*BLOCK_SIZE; tx++) {
+          if (tx < BLOCK_SIZE) { //peri-row
+            int idx=tx;
+            for(int i=1; i < BLOCK_SIZE; i++){
+              for (int j=0; j < i; j++)
+                peri_row[i * BLOCK_SIZE + idx]-=dia[i * BLOCK_SIZE+ j]*peri_row[j * BLOCK_SIZE + idx];
+            }
+          } else { //peri-col
+            int idx=tx - BLOCK_SIZE;
+            for(int i=0; i < BLOCK_SIZE; i++){
+              for(int j=0; j < i; j++)
+                peri_col[idx * BLOCK_SIZE + i]-=peri_col[idx * BLOCK_SIZE+ j]*dia[j * BLOCK_SIZE + i];
+              peri_col[idx * BLOCK_SIZE + i] /= dia[i * BLOCK_SIZE+ i];
+            }
+          }
+        }
+
+        #pragma acc loop vector
+        for (int tx = 0; tx < 2*BLOCK_SIZE; tx++) {
+          size_t array_offset;
+          if (tx < BLOCK_SIZE) { //peri-row
+            int idx=tx;
+            array_offset = (offset+1)*(size_t)matrix_dim+offset;
+            for(int i=1; i < BLOCK_SIZE; i++){
+              m[array_offset+(bx+1)*BLOCK_SIZE+idx] = peri_row[i*BLOCK_SIZE+idx];
+              array_offset += matrix_dim;
+            }
+          } else { //peri-col
+            int idx=tx - BLOCK_SIZE;
+            array_offset = (offset+(bx+1)*BLOCK_SIZE)*(size_t)matrix_dim+offset;
+            for(int i=0; i < BLOCK_SIZE; i++){
+              m[array_offset+idx] =  peri_col[i*BLOCK_SIZE+idx];
+              array_offset += matrix_dim;
+            }
+          }
+        }
       }
     }
 
-    #pragma acc parallel teams num_teams(((matrix_dim-i)/BLOCK_SIZE-1) * ((matrix_dim-i)/BLOCK_SIZE-1)) \
-                              thread_limit(BLOCK_SIZE*BLOCK_SIZE)
     {
-      float peri_row[BLOCK_SIZE * BLOCK_SIZE];
-      float peri_col[BLOCK_SIZE * BLOCK_SIZE];
-      #pragma omp parallel
-      {
-        int  bx = omp_get_team_num() % ((matrix_dim-i)/BLOCK_SIZE-1); // item.get_group(1);  
-        int  by = omp_get_team_num() / ((matrix_dim-i)/BLOCK_SIZE-1); //omp_get_team_num();  
-        
-        int  tx = omp_get_thread_num() % BLOCK_SIZE; //item.get_local_id(1);
-        int  ty = omp_get_thread_num() / BLOCK_SIZE; //omp_get_thread_num();
+      int num_int = (matrix_dim-i)/BLOCK_SIZE-1;
+      #pragma acc parallel loop gang num_gangs(num_int*num_int) vector_length(BLOCK_SIZE*BLOCK_SIZE) present(m)
+      for (int blk = 0; blk < num_int*num_int; blk++) {
+        float peri_row[BLOCK_SIZE * BLOCK_SIZE];
+        float peri_col[BLOCK_SIZE * BLOCK_SIZE];
 
-        int i;
-        float sum;
+        int  bx = blk % num_int;
+        int  by = blk / num_int;
 
         int global_row_id = offset + (by+1)*BLOCK_SIZE;
         int global_col_id = offset + (bx+1)*BLOCK_SIZE;
 
-        peri_row[ty * BLOCK_SIZE + tx] = m[(offset+ty)*(size_t)matrix_dim+global_col_id+tx];
-        peri_col[ty * BLOCK_SIZE + tx] = m[(global_row_id+ty)*(size_t)matrix_dim+offset+tx];
+        #pragma acc loop vector
+        for (int tid = 0; tid < BLOCK_SIZE*BLOCK_SIZE; tid++) {
+          int  tx = tid % BLOCK_SIZE;
+          int  ty = tid / BLOCK_SIZE;
 
-        #pragma omp barrier
+          peri_row[ty * BLOCK_SIZE + tx] = m[(offset+ty)*(size_t)matrix_dim+global_col_id+tx];
+          peri_col[ty * BLOCK_SIZE + tx] = m[(global_row_id+ty)*(size_t)matrix_dim+offset+tx];
+        }
 
-        sum = 0;
-        for (i=0; i < BLOCK_SIZE; i++)
-          sum += peri_col[ty * BLOCK_SIZE + i] * peri_row[i * BLOCK_SIZE + tx];
-        m[(global_row_id+ty)*(size_t)matrix_dim+global_col_id+tx] -= sum;
+        #pragma acc loop vector
+        for (int tid = 0; tid < BLOCK_SIZE*BLOCK_SIZE; tid++) {
+          int  tx = tid % BLOCK_SIZE;
+          int  ty = tid / BLOCK_SIZE;
+
+          float sum = 0;
+          for (int i=0; i < BLOCK_SIZE; i++)
+            sum += peri_col[ty * BLOCK_SIZE + i] * peri_row[i * BLOCK_SIZE + tx];
+          m[(global_row_id+ty)*(size_t)matrix_dim+global_col_id+tx] -= sum;
+        }
       }
     }
   } // for
 
-  offset = i;  // add the offset 
-  #pragma acc parallel teams gang(1) thread_limit(BLOCK_SIZE)
+  offset = i;  // add the offset
+  #pragma acc parallel num_gangs(1) vector_length(BLOCK_SIZE) present(m)
   {
     float shadow[BLOCK_SIZE * BLOCK_SIZE];
-    #pragma omp parallel
-    {
-      int i,j;
-      int tx = omp_get_thread_num() ;
-    
+
+    #pragma acc loop vector
+    for (int tx = 0; tx < BLOCK_SIZE; tx++) {
       size_t array_offset = offset*(size_t)matrix_dim+offset;
-      for(i=0; i < BLOCK_SIZE; i++){
+      for(int i=0; i < BLOCK_SIZE; i++){
         shadow[i * BLOCK_SIZE + tx]=m[array_offset + tx];
         array_offset += matrix_dim;
       }
-      
-      #pragma omp barrier
-      
-      for(i=0; i < BLOCK_SIZE-1; i++) {
+    }
+
+    for(int i=0; i < BLOCK_SIZE-1; i++) {
+      #pragma acc loop vector
+      for (int tx = 0; tx < BLOCK_SIZE; tx++) {
         if (tx>i) {
-          for(j=0; j < i; j++)
+          for(int j=0; j < i; j++)
             shadow[tx * BLOCK_SIZE + i] -= shadow[tx * BLOCK_SIZE + j] * shadow[j * BLOCK_SIZE + i];
           shadow[tx * BLOCK_SIZE + i] /= shadow[i * BLOCK_SIZE + i];
         }
-    
-        #pragma omp barrier
+      }
+
+      #pragma acc loop vector
+      for (int tx = 0; tx < BLOCK_SIZE; tx++) {
         if (tx>i){
-          for(j=0; j < i+1; j++)
+          for(int j=0; j < i+1; j++)
             shadow[(i+1) * BLOCK_SIZE + tx] -= shadow[(i+1) * BLOCK_SIZE + j]*shadow[j * BLOCK_SIZE + tx];
         }
-
-        #pragma omp barrier
       }
-    
-      array_offset = (offset+1)*(size_t)matrix_dim+offset;
-      for(i=1; i < BLOCK_SIZE; i++){
+    }
+
+    #pragma acc loop vector
+    for (int tx = 0; tx < BLOCK_SIZE; tx++) {
+      size_t array_offset = (offset+1)*(size_t)matrix_dim+offset;
+      for(int i=1; i < BLOCK_SIZE; i++){
         m[array_offset+tx]=shadow[i * BLOCK_SIZE + tx];
         array_offset += matrix_dim;
       }
@@ -316,7 +328,7 @@ int main ( int argc, char *argv[] )
    auto end = std::chrono::steady_clock::now();
    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
    printf("Total kernel execution time : %f (s)\n", time * 1e-9f);
-  } // #pragma acc parallel  data map
+  } // #pragma acc parallel loop  data map
 
   /* end of timing point */
   stopwatch_stop(&sw);
